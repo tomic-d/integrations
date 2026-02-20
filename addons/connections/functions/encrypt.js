a49a9f6b08e8270f201f1a3f0b34366e67127b12
Dejan Tomic <dejan@192.168.1.4>
2026-02-20 13:06:07 +0100

    use framework Encrypt/Decrypt, bump divhunt to 2.0.2

--- addons/connections/functions/encrypt.js
+++ addons/connections/functions/encrypt.js
@@ -1,16 +1,7 @@
-import crypto from 'crypto';
+import divhunt from 'divhunt';
 import connections from '#connections/addon.js';
 
 connections.Fn('encrypt', function(data)
 {
-    const key = Buffer.from(process.env.ENCRYPTION_KEY, 'hex');
-    const iv = crypto.randomBytes(12);
-    const cipher = crypto.createCipheriv('aes-256-gcm', key, iv);
-
-    let encrypted = cipher.update(JSON.stringify(data), 'utf8', 'base64');
-    encrypted += cipher.final('base64');
-
-    const tag = cipher.getAuthTag();
-
-    return iv.toString('base64') + ':' + encrypted + ':' + tag.toString('base64');
+    return divhunt.Encrypt(data, process.env.ENCRYPTION_KEY);
 });
